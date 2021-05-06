4e1b77a8a74403feb5cf7dabc874b3b3bd2044d2
OlgaSeilah <princenoru@gmail.com>
2021-05-06 15:15:27 +0300

    first

--- src/test/java/ru/training/at/hw2/Exercise_1.java
+++ src/test/java/ru/training/at/hw2/Exercise_1.java
@@ -1,18 +1,20 @@
 package ru.training.at.hw2;
 
+
 import com.epam.tat.module4.Calculator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Exercise_1 {
-    Calculator calculator = new Calculator();
 
-    @BeforeClass
-    public void setup() {
+    @Test
+    public void setupDriver() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Seilah/IdeaProjects/chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
-
-
+        driver.get("https://www.google.com/");
 
     }
 
